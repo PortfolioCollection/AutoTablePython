5dd0d002f2d0e6a3e931b335cf75e2b1f8eb8276
Michael Muratov <michael.muratov@mail.utoronto.ca>
2018-07-17 16:24:49 -0400

    Create Builder.py

---
+++ Builder.py
@@ -0,0 +1,114 @@
+from Course import*
+
+class Builder:
+    def __init__(self, AutoTable):
+        self.AutoTable = AutoTable
+
+
+    def build_table(self):
+        csc324 = Course("csc324")
+        lec101 = Lecture("0101","Wed",13,15)
+        lec102 = Lecture("0102","Wed",17,19)
+        pra101 = Practical("0101","Fri",11,12)
+        pra102 = Practical("0102","Fri",11,12)
+        pra103 = Practical("0103","Fri",12,13)
+        pra104 = Practical("0104","Fri",12,13)
+        pra105 = Practical("0105","Fri",13,14)
+        csc324.add_section(lec101)
+        csc324.add_section(lec102)
+        csc324.add_section(pra101)
+        csc324.add_section(pra102)
+        csc324.add_section(pra103)
+        csc324.add_section(pra104)
+        csc324.add_section(pra105)
+        
+        csc347 = Course("csc347")
+        lec101 = Lecture("0101","Fri",15,17)
+        pra101 = Practical("0101","Wed",11,12)
+        pra102 = Practical("0102","Wed",15,16)
+        csc347.add_section(lec101)
+        csc347.add_section(pra101)
+        csc347.add_section(pra102)
+
+        csc369 = Course("csc369")
+        lec101 = Lecture("0101","Mon",9,11)
+        lec102 = Lecture("0102","Mon",13,15)
+        pra101 = Practical("0101","Thu",9,10)
+        pra102 = Practical("0102","Thu",9,10)
+        pra103 = Practical("0103","Thu",10,11)
+        pra104 = Practical("0104","Thu",10,11)
+        csc369.add_section(lec101)
+        csc369.add_section(lec102)
+        csc369.add_section(pra101)
+        csc369.add_section(pra102)
+        csc369.add_section(pra103)
+        csc369.add_section(pra104)
+
+        mat301 = Course("mat301")
+        lec101 = Lecture("0101","Wed",11,13)
+        lec102 = Lecture("0101","Fri",9,10)
+        tut101 = Tutorial("0101","Wed",9,10)
+        tut102 = Tutorial("0102","Wed",15,11)
+        tut103 = Tutorial("0103","Wed",13,14)
+        mat301.add_section(lec101)
+        mat301.add_section(lec102)
+        mat301.add_section(tut101)
+        mat301.add_section(tut102)
+        mat301.add_section(tut103)
+
+        csc338 = Course("csc338")
+        lec101 = Lecture("0101","Wed",15,17)
+        tut101 = Tutorial("0101","Fri",13,14)
+        tut102 = Tutorial("0102","Fri",14,15)
+        csc338.add_section(lec101)
+        csc338.add_section(tut101)
+        csc338.add_section(tut102)
+
+        csc343 = Course("csc343")
+        lec101 = Lecture("0101","Mon",9,11)
+        pra101 = Practical("0101","Thu",13,14)
+        pra102 = Practical("0102","Wed",14,15)
+        csc343.add_section(lec101)
+        csc343.add_section(pra101)
+        csc343.add_section(pra102)
+
+        csc363 = Course("csc363")
+        lec101 = Lecture("0101","Mon",11,13)
+        lec102 = Lecture("0102","Thu",18,20)
+        tut101 = Tutorial("0101","Wed",13,14)
+        tut102 = Tutorial("0102","Wed",13,14)
+        tut103 = Tutorial("0103","Wed",14,15)
+        tut104 = Tutorial("0104","Wed",14,15)
+        csc363.add_section(lec101)
+        csc363.add_section(lec102)
+        csc363.add_section(tut101)
+        csc363.add_section(tut102)
+        csc363.add_section(tut103)
+        csc363.add_section(tut104)
+
+        csc384 = Course("csc384")
+        lec101 = Lecture("0101","Wed",11,13)
+        tut101 = Tutorial("0101","Tue",16,17)
+        tut102 = Tutorial("0102","Tue",17,18)
+        tut103 = Tutorial("0103","Tue",17,18)
+        csc384.add_section(lec101)
+        csc384.add_section(tut101)
+        csc384.add_section(tut102)
+        csc384.add_section(tut103)
+        
+    
+        self.AutoTable.add_course(csc324)
+        self.AutoTable.add_course(csc347)
+        self.AutoTable.add_course(csc369)
+        self.AutoTable.add_course(mat301)
+
+        self.AutoTable.add_course(csc338)
+        self.AutoTable.add_course(csc343)
+        self.AutoTable.add_course(csc363)
+        self.AutoTable.add_course(csc384)
+        
+
+
+
+        return self.AutoTable
+        
